2bc1796c82e1409189caba5e6d52652bd6138af3
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2022-03-18 16:01:18 +0530

    Add files via upload

---
+++ sum&average.py
@@ -0,0 +1,12 @@
+listt = [1,2,3,4,5,6,7,8,9,10]
+
+count = 0
+
+for i in listt:
+
+    count += i
+
+    avg = count/len(listt)
+    
+    print("The sum is: ", count)
+    print("The average is: ", avg)

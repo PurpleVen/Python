3d296e75ebbd4c14d6571c5177861ab12885333b
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2022-05-01 13:34:08 +0530

    Add files via upload

---
+++ IndexError&ZeroDivisionError.py
@@ -0,0 +1,12 @@
+#53_IT_Vendra
+a = 100
+b = 0
+
+c = a/b
+print(c)
+
+# myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print("The list is:", myList)
+# index = 10
+# element = myList[index]
+# print("Element at index {} is {}".format(index,element))

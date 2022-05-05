aeb1110b4167d41a87c038e3dbe8852ddf4e3057
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2022-05-05 11:40:20 +0530

    Add files via upload

---
+++ IndexError.py
@@ -0,0 +1,7 @@
+#53_IT_Vendra Question[4-1a]
+
+myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("The list is:", myList)
+index = int(input("Enter the index number: "))
+element = myList[index]
+print("Element at index {} is {}".format(index,element))

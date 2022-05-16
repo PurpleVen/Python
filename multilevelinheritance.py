938d6c608ed38e064d46b296231bcc704419247c
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2022-05-16 20:34:36 +0530

    Add files via upload

--- multilevelinheritance.py
+++ multilevelinheritance.py
@@ -1,30 +1,59 @@
+# class A:
+#     def __init__(self):
+#         print('Initializing: class A')
+#
+#     def sub_method(self, b):
+#         print('Printing from class A:', b)
+#
+#
+# class B(A):
+#     def __init__(self):
+#         print('Initializing: class B')
+#         super().__init__()
+#
+#     def sub_method(self, b):
+#         print('Printing from class B:', b)
+#         super().sub_method(b + 1)
+#
+#
+# class C(B):
+#     def __init__(self):
+#         print('Initializing: class C')
+#         super().__init__()
+#
+#     def sub_method(self, b):
+#         print('Printing from class C:', b)
+#         super().sub_method(b + 1)
+#
+#
+# if __name__ == '__main__':
+#     c = C()
+#     c.sub_method(1)
+
 class A:
     def __init__(self):
-        print('Initializing: class A')
+        print("Initialoze from class A")
 
     def sub_method(self, b):
-        print('Printing from class A:', b)
-
+        print("I am in class A with my value as: ",b)
 
 class B(A):
     def __init__(self):
-        print('Initializing: class B')
+        print("Initialize form class B")
         super().__init__()
 
     def sub_method(self, b):
-        print('Printing from class B:', b)
-        super().sub_method(b + 1)
-
+        print("I am in class B with Value: ",b)
+        super().sub_method(b+1)
 
 class C(B):
     def __init__(self):
-        print('Initializing: class C')
+        print("Initalize class C")
         super().__init__()
 
     def sub_method(self, b):
-        print('Printing from class C:', b)
-        super().sub_method(b + 1)
-
+        print("The value of C is: ",b)
+        super().sub_method(b+1)
 
 if __name__ == '__main__':
     c = C()

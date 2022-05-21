04b38790cff906e16b707025624796bb2af489cc
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2022-05-21 15:43:16 +0530

    Add files via upload

--- DivisibleBy4.py
+++ DivisibleBy4.py
@@ -1,5 +1,14 @@
-lower = 1
-upper = 100
+#53_IT_Vendra, Question[1-4]
+lower = 100
+upper = 200
 for i in range(lower, upper+1):
    if(i%4==0):
-      print(i)
+      print("Divisble by 4:",i)
+
+print("\n")
+print("==========================")
+print("\n")
+
+for j in range(lower, upper+1):
+   if(j%7==0):
+      print("Divisible by 7:",j)
